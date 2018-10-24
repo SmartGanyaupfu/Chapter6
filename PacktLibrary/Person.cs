67fe834cadec43983214a42068a7ed72b3738d65
SmartGanyaupfu <34765370+SmartGanyaupfu@users.noreply.github.com>
2018-10-24 09:26:24 +0200

    Updated

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+
+namespace Packt.CS7
+{
+    public partial class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born {DateOfBirth:dddd, d MMMM yyyy}");
+        }
+
+        public static  Person ProCreate( Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"baby of {p1.Name} and {p2.Name}"
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+    
+        }
+
+        public Person ProCreateWith(Person partner)
+        {
+            return ProCreate(this, partner);
+        }
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using Packt.CS7;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Smart!");
+            var smart = new Person { Name = "Smart" };
+            var mobbby = new Person { Name = "Mobby" };
+            var baShane = new Person { Name = "baShane" };
+
+            var shane = smart.ProCreateWith(mobbby);
+
+            WriteLine($"{smart.Name} has {smart.Children.Count} children");
+            WriteLine($"{mobbby.Name} has {mobbby.Children.Count} children");
+            WriteLine($"{baShane.Name} has {baShane.Children.Count} children");
+            WriteLine($"{smart.Name}' is named {smart.Children[0].Name }");
+
+
+        }
+    }
+}
